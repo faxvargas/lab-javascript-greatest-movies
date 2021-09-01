86351cb7b6445e7b6cccf9585362adcfdf4c0322
faxvargas <fab.med1011@gmail.com>
2021-09-01 08:55:32 -0500

    terminado hasta la 5, la iteracion 6 supe como hacerlo

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,74 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
-
+function getAllDirectors(pelis) {
+  let ListaDeDirectores = pelis.map((person) => {
+    return person.director
+  } )
+  return ListaDeDirectores
+}
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arr) {
+  const stevenpelis = arr.filter(
+    (movie) =>
+      movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+  );
+  return stevenpelis.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(moviess) {
+  if(moviess == false){
+    return 0
+  }
+  const num = moviess.reduce((result, valor) =>{
+    if (valor.score) {
+      return result + valor.score;
+    }else {
+      return result;
+    }
+  }, 0);
+  return Math.round((num / moviess.length) * 100) / 100;
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(peliculas) {
+    if (peliculas == false) {
+      return 0;
+    }
+    let count = 0;
+    const num = peliculas.reduce((result, value) =>{
+      
+      if (value.genre.includes('Drama') === true) {
+        count++;
+        return result + value.score;
+      }else {
+        return result;
+      }
+    }, 0);
+    if (count == 0) {
+      return 0;
+    }else {
+      return Math.round((num / count) * 100) / 100;
+    }
+  }
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(moviesArr) {
+  if (!moviesArr.length) {
+    return 0;
+  }
+  const segArr = moviesArr.sort((x, y) => {
+    if (x.year === y.year) {
+      return x.title.localeCompare(y.title);
+    }
+    return x.year - y.year;
+  });
+  return segArr;
+}
+
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
